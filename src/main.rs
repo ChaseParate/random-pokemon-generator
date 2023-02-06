ac22398226c7ace4d49ebf836085ca0f9f8f805f
ChaseParate <97500441+ChaseParate@users.noreply.github.com>
2023-02-06 13:09:04 -0700

    Get random pokemon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use rustemon::client::{CacheMode, Environment, RustemonClientBuilder};
+
+mod pokemon;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
+    let rustemon_client = RustemonClientBuilder::default()
+        .with_environment(Environment::Production)
+        .with_mode(CacheMode::ForceCache)
+        .try_build()?;
+
+    let species = pokemon::get_random_pokemon_species(&rustemon_client).await?;
+    let name = pokemon::get_pokemon_species_name(&species).await;
+    println!("{}", name);
+
+    let default_sprite_url =
+        pokemon::get_pokemon_species_default_sprite_url(&rustemon_client, &species).await?;
+    println!("{}", default_sprite_url.unwrap());
 
     Ok(())
 }
---
+++ src/pokemon.rs
@@ -0,0 +1,43 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use rustemon::client::RustemonClient;
+use rustemon::error::Error;
+use rustemon::model::pokemon::PokemonSpecies;
+use rustemon::pokemon::pokemon_species;
+use rustemon::Follow;
+
+pub async fn get_random_pokemon_species(
+    rustemon_client: &RustemonClient,
+) -> Result<PokemonSpecies, Error> {
+    let mut rng = thread_rng();
+    let all_pokemon_species = pokemon_species::get_all_pages(rustemon_client).await?;
+
+    let species_resource = all_pokemon_species.choose(&mut rng).unwrap();
+    let species = species_resource.follow(rustemon_client).await?;
+
+    Ok(species)
+}
+
+pub async fn get_pokemon_species_name(species: &PokemonSpecies) -> String {
+    let name = species
+        .names
+        .iter()
+        .find(|name| name.language.name == "en")
+        .unwrap();
+
+    name.name.clone()
+}
+
+pub async fn get_pokemon_species_default_sprite_url(
+    rustemon_client: &RustemonClient,
+    species: &PokemonSpecies,
+) -> Result<Option<String>, Error> {
+    let variety = species
+        .varieties
+        .iter()
+        .find(|variety| variety.is_default)
+        .unwrap();
+    let pokemon = variety.pokemon.follow(&rustemon_client).await?;
+
+    Ok(pokemon.sprites.front_default)
+}
