a0ccc4df79cb64320d8ad8731d8527b0b237c93b
ChaseParate <me@chasehark.com>
2024-10-17 10:53:47 -0600

    Run `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::io::{self, Write};
 use image::{DynamicImage, GenericImageView};
 use rustemon::client::{CacheMode, Environment, RustemonClientBuilder};
 
-mod sprite;
 mod pokemon;
+mod sprite;
 
 fn clear_screen() {
     print!("\x1B[2J\x1B[1;1H");
@@ -35,10 +35,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let species = pokemon::get_random_pokemon_species(&rustemon_client).await?;
 
-    let sprite_url =
-        pokemon::get_pokemon_species_sprite_url(&rustemon_client, &species)
-            .await?
-            .unwrap();
+    let sprite_url = pokemon::get_pokemon_species_sprite_url(&rustemon_client, &species)
+        .await?
+        .unwrap();
     let sprite = sprite::crop_to_content(&sprite::download_from_url(&sprite_url).await?);
 
     clear_screen();
